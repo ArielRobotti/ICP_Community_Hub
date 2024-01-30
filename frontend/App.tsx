9845b8a8ae1ac5c777474f760438f627d2161658
Daniel Arroyo <daniel0ar@hotmail.com>
2024-01-29 22:57:45 -0500

    app(feat): Add routing with react router

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -7,6 +7,8 @@ import { createClient } from "@connect2ic/core"
 import { defaultProviders } from "@connect2ic/core/providers"
 import { ConnectButton, ConnectDialog, Connect2ICProvider } from "@connect2ic/react"
 import "@connect2ic/core/style.css"
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 /*
  * Import canister definitions like this:
  */
@@ -14,33 +16,17 @@ import "@connect2ic/core/style.css"
 /*
  * Some examples to get you started
  */
-import { Transfer } from "./components/Transfer"
 import { Profile } from "./components/Profile"
 
 function App() {
   return (
-    <div className="App">
-      <div className="auth-section">
-        <ConnectButton />
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" Component={Profile}></Route>
+        </Routes>
       </div>
-      <ConnectDialog />
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="slogan">
-          React+TypeScript Template
-        </p>
-        <p className="twitter">by <a href="https://twitter.com/miamaruq">@miamaruq</a></p>
-      </header>
-
-      <p className="examples-title">
-        Examples
-      </p>
-      <div className="examples">
-        <Profile />
-        <Transfer />
-      </div>
-    </div>
+    </Router>
   )
 }
 
