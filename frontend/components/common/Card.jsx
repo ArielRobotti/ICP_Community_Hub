2e56958ee584bf6602d1268ba7feac24826e8c89
Daniel Arroyo <daniel0ar@hotmail.com>
2024-02-11 11:39:20 -0500

    incoming(feat): Add aproval functions

--- frontend/components/common/Card.jsx
+++ frontend/components/common/Card.jsx
@@ -1,8 +1,8 @@
 import React from "react"
 
-const Card = ({ title, description, author, readTime }) => {
+const Card = ({ title, description, author, readTime, onClick, footer }) => {
   return (
-    <div className="w-full lg:max-w-full lg:flex">
+    <div className="w-full lg:max-w-full lg:flex" onClick={onClick}>
       <div className="border border-gray-400  bg-white dark:bg-black rounded-lg p-4 flex flex-col justify-between leading-normal">
         <div className="mb-8">
           <div className="text-gray-900 font-bold text-xl mb-2">{title}</div>
@@ -14,6 +14,7 @@ const Card = ({ title, description, author, readTime }) => {
             <p className="text-gray-600">{readTime} hours</p>
           </div>
         </div>
+        {footer}
       </div>
     </div>
   )
--- frontend/pages/tutorials/incoming.tsx
+++ frontend/pages/tutorials/incoming.tsx
@@ -4,25 +4,62 @@ import { Tutorial } from "./types"
 import { useCanister } from "@connect2ic/react"
 
 const Incoming = () => {
-  const [tutorials, setTutorials] = useState<Tutorial[]>([])
-  const [backend] = useCanister("backend")
+  const [tutorials, setTutorials] = useState<Tutorial[]>([]);
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [backend] = useCanister("backend");
 
   const fetchTutorials = async () => {
-    const res = (await backend.getIncomingPublication()) as Tutorial[]
-    console.log("Publications are: ", res)
-    setTutorials(res)
-  }
+    const res = (await backend.getIncomingPublication()) as Tutorial[];
+    console.log("Publications are: ", res);
+    setTutorials(res);
+  };
+
+  const handleApproval = async (tutoId: number) => {
+    const res = await backend.aprovePublication(tutoId);
+    if (res){
+      console.log("Tutorial aproved", res);
+    }
+    else {
+      console.log("Approve failed");
+    }
+  };
+
+  const handleVote = async (tutoId: number, vote: boolean) => {
+    const res = await backend.votePublication(tutoId, vote);
+    if (res){
+      console.log("Tutorial voted", res);
+    }
+    else {
+      console.log("Voting failed");
+    }
+  };
+
+  const adminFooter = (tutoId: number) => {
+    return (
+      <button onClick={() => handleApproval(tutoId)}>Approve</button>
+    );
+  };
+
+
+  const daoMemberFooter = (index: number) => {
+    return (
+      <div className="flex flex-row justify-between">
+        <button onClick={() => handleVote(index, true)}>Vote Yes</button>
+        <button onClick={() => handleVote(index, false)}>Vote no</button>
+      </div>
+    );
+  };
 
   useEffect(() => {
-    fetchTutorials()
+    fetchTutorials();
   }, [])
 
   return (
     <div className="p-10 min-h-[90vh]">
       <div className="flex flex-row justify-between w-100">
-        <h1 className="text-3xl font-bold">Todos los tutoriales</h1>
+        <h1 className="text-3xl font-bold">Tutorials pending approvals</h1>
         <button className="rounded-full bg-indigo-600 px-3.5 py-2.5 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-          Nuevo
+          New
         </button>
       </div>
       <div className="flex flex-col gap-5 pt-5">
@@ -33,7 +70,8 @@ const Incoming = () => {
             description={tutorial?.description}
             author={tutorial?.author}
             readTime={5}
-          ></Card>
+            onClick= {() => {}}
+            footer={isAdmin ? adminFooter : daoMemberFooter}></Card>
         ))}
       </div>
     </div>
